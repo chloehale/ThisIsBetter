f633a2dd57fcad23d8556222904c6def65fc961b
Stephen Jensen <steve@Stephens-Air-2.app.byu.edu>
2016-03-15 16:59:56 -0600

    filled out create events page including calendar view. Updated color scheme included

--- ThisIsBetter/app/src/main/java/s3/thisisbetter/CreateEventActivity.java
+++ ThisIsBetter/app/src/main/java/s3/thisisbetter/CreateEventActivity.java
@@ -1,12 +1,24 @@
 package s3.thisisbetter;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class CreateEventActivity extends AppCompatActivity {
+
+    private MaterialCalendarView mCalView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,10 @@ public class CreateEventActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Here are the dates we'll need to pass into the availability input
+                List<CalendarDay> dates = mCalView.getSelectedDates();
+
                 Intent intent = new Intent(CreateEventActivity.this, AvailabilityInputActivity.class);
                 startActivity(intent);
             }
@@ -30,6 +46,12 @@ public class CreateEventActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        //TODO: Add functionality to the event title input text
+        EditText eventTitle = (EditText) findViewById(R.id.event_title);
+
+        mCalView = (MaterialCalendarView) findViewById(R.id.calendarView);
+        mCalView.setSelectionMode(MaterialCalendarView.SELECTION_MODE_MULTIPLE);
 
     }
 }
