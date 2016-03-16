faa6820882b38313b885ef7693cd94c5c8ac6108
Paul Soderquist <paulsoderquist3@gmail.com>
2016-03-16 12:00:15 -0600

    Made navigation to invite screen

--- ThisIsBetter/app/src/main/java/s3/thisisbetter/AvailabilityInputActivity.java
+++ ThisIsBetter/app/src/main/java/s3/thisisbetter/AvailabilityInputActivity.java
@@ -1,7 +1,14 @@
 package s3.thisisbetter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+
+import java.util.List;
 
 public class AvailabilityInputActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class AvailabilityInputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availability_input);
+
+
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AvailabilityInputActivity.this, InviteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
