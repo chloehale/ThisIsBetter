926ff1dbbadb16e1249ca16258635daa4e370db7
Stephen Jensen <steve@Stephens-MacBook-Air-2.local>
2016-03-16 23:47:38 -0600

    updated action bar layout for availability input activity and functional cancel button

--- ThisIsBetter/app/src/main/java/s3/thisisbetter/AvailabilityInputActivity.java
+++ ThisIsBetter/app/src/main/java/s3/thisisbetter/AvailabilityInputActivity.java
@@ -1,9 +1,13 @@
 package s3.thisisbetter;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -12,11 +16,28 @@ import java.util.List;
 
 public class AvailabilityInputActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availability_input);
 
+        //Change the color of the status bar if the version > lollipop
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+        Button cancelButton = (Button) findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         Button nextButton = (Button) findViewById(R.id.next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +47,10 @@ public class AvailabilityInputActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
 
