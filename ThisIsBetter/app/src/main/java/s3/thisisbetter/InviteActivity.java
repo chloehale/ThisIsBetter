35c49a76fa0541b70ac2ade75ae458cb980c1819
Paul Soderquist <paulsoderquist3@gmail.com>
2016-03-17 20:58:19 -0600

    added enter team member email address, and finish goes to home activity

--- ThisIsBetter/app/src/main/java/s3/thisisbetter/InviteActivity.java
+++ ThisIsBetter/app/src/main/java/s3/thisisbetter/InviteActivity.java
@@ -1,6 +1,9 @@
 package s3.thisisbetter;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -9,8 +12,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -27,6 +33,7 @@ public class InviteActivity extends AppCompatActivity {
     int clickCounter=0;
 
     ListView listView;
+    String m_Text;
 
 
     @Override
@@ -40,15 +47,51 @@ public class InviteActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
 
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         Drawable fabDr= fab.getDrawable();
         DrawableCompat.setTint(fabDr, Color.WHITE);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                addItems(view);
+            public void onClick(final View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(InviteActivity.this);
+                builder.setTitle("Add an email");
+
+                // Set up the input
+                final EditText input = new EditText(InviteActivity.this);
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        m_Text = input.getText().toString();
+                        addItems(view);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
             }
         });
+
+        Button finishBtn = (Button) findViewById(R.id.button);
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InviteActivity.this, TabbedEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
